e48131737acab3f353308f22f21b749cbf0c62d4
Se jeong Kim <somsombenji@naver.com>
2020-05-27 22:27:18 +0900

    variable change

--- funccrud/views.py
+++ funccrud/views.py
@@ -16,27 +16,25 @@ def read(request):
     return render(request, 'funccrud/funccrud.html', {'blogs':blogs})
 
 def create(request): #역할 1. 새로운 블로그 글 저장POST   2. 글쓰기 페이지를 띄워줌GET(즉!=POST)
-    
     if request.method == 'POST':
-        form = NewBlog(request.POST)
-        if form.is_valid:
-            post = form.save(commit=False)
+        form_content = NewBlog(request.POST)
+        if form_content.is_valid:
+            post = form_content.save(commit=False)
             post.pub_date = timezone.now()
             post.save()
             return redirect('home')
     else:
-        form = NewBlog()
-        return render(request, 'funccrud/new.html', {'form':form})
+        form_content = NewBlog()
+        return render(request, 'funccrud/new.html', {'formK':form_content})
 
 
 def update(request, pk): #어떤 블로그 수정할지 객체 가져오기, 해당하는 블로그 객체의 입력공간
-
     blog = get_object_or_404(Blog, pk=pk)
-    form = NewBlog(request.POST, instance=blog)
-    if form.is_valid():
-        form.save()
+    form_content = NewBlog(request.POST, instance=blog)
+    if form_content.is_valid():
+        form_content.save()
         return redirect('home')
-    return render(request, 'funccrud/new.html', {'form':form})
+    return render(request, 'funccrud/new.html', {'formK':form_content})
 
 def delete(request, pk):
     blog = get_object_or_404(Blog, pk = pk)
